86ce1e857cbdd9ed2682628033178bb60d48f30e
Yash Sanghvi <50194122+sanghvi-yash@users.noreply.github.com>
2021-04-07 22:28:58 +0530

    Adding files

---
+++ service_reusing_layer/handler.py
@@ -0,0 +1,18 @@
+import json
+import numpy as np
+
+
+def hello(event, context):
+    print(np.random.randint(5))
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
